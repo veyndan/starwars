47c23a4f1da6b5934847ed47aa471cfd9edc7e44
veyndan <veyndan@gmail.com>
2019-10-17 12:53:38 +0100

    Remove redundant toolbar

--- app/src/main/java/com/veyndan/starwars/MainActivity.kt
+++ app/src/main/java/com/veyndan/starwars/MainActivity.kt
@@ -2,16 +2,11 @@ package com.veyndan.starwars
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.setupWithNavController
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        toolbar.setupWithNavController(findNavController(R.id.nav_host_fragment))
     }
 }
